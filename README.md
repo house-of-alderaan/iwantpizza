c72f44337eb53b989672bb758219f9dfc8a9b8d1
Carlos Mañas <carlos@spacenomads.com>
2019-11-18 14:41:38 +0100

    Add react project structure

---
+++ README.md
@@ -0,0 +1,5 @@
+# I Want Pizza 🍕
+
+---
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+   🍕Yay!   
+    </div>
+  );
+}
+
+export default App;
