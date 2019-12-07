8e255e78cda916518e5272b70433234b81158361
Carlos Mañas <carlos@spacenomads.com>
2019-12-07 12:39:38 +0100

    Change indentation mode from "Loser" to "correct"

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './App.scss';
 
 function App() {
-  return (
-    <div className="app">
-  		 <span role="img" aria-label="Pizza">🍕</span> Yay!
-    </div>
-  );
+	return (
+		<div className="app">
+		<span role="img" aria-label="Pizza">🍕</span> Yay!
+		</div>
+	);
 }
 
 export default App;
